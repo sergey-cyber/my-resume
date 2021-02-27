c32ff3c9ca074a13c5ee7fc7854131c8508de49c
Sergey <68175966+sergey-cyber@users.noreply.github.com>
2021-02-27 16:37:08 +0800

    Update burgerMenu.js

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -5,4 +5,8 @@ document.querySelector('.burger').addEventListener('click', function(){
         document.querySelector('.menuWrapper').classList.toggle("active");
     },700) */  
 });
-
+//For touch devices
+document.querySelector('.burger').addEventListener('touchstart', function(){
+    document.querySelector('.burger span').classList.toggle('active');
+    document.querySelector('.menu').classList.toggle("animate"); 
+});
