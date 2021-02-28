6d14988d627c1847359d2d7bf2b3f4cb049b18ae
Sergey <68175966+sergey-cyber@users.noreply.github.com>
2021-02-28 09:45:37 +0800

    Update preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -1,4 +1,4 @@
 window.onload = () => {
-    document.querySelector('.preloader').style.display = 'none';
-    typeIt.go();
+    document.querySelector('.preloader').style.display = 'none';    //Delete preloader
+    typeIt.go(); //Started autoprint text on about me page
 }
